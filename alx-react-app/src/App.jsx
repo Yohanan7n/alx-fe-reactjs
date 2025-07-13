1c81c669d3cb93cd2ffdbfc5620236b56277740c
Yohanan7n <156466410+Yohanan7n@users.noreply.github.com>
2025-07-13 20:39:28 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <WelcomeMessage />
-    </>
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
   );
 }
 
