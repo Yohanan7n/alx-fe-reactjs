fbf0c72af6344d79a2ae3cb3a6d4c18b71cc3895
Yohanan7n <156466410+Yohanan7n@users.noreply.github.com>
2025-07-20 22:02:46 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,10 +3,15 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
 
 function App() {
   return (
-    <div>
+   <div>
+       <div className="App">
+      <Counter />
+    </div>
       <Header />
       <MainContent />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
