b972699ae460340d3df388ba7fce326ce3f59627
Yohanan7n <156466410+Yohanan7n@users.noreply.github.com>
2025-07-20 21:05:12 +0300

    Create  MainContent.jsx

---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#e6f2ff', minHeight: '300px' }}>
+      <p style={{ fontSize: '18px' }}>Explore the list of amazing cities below!</p>
+    </main>
+  );
+}
+
+export default MainContent;
